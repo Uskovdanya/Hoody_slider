b0b494bb764f2e87d68079f3bdf30db8426143b7
Daniil <uskov.danya@gmail.com>
2022-09-26 05:36:59 +0300

    second commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+	const showSlider = new Swiper ('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800,
+		centeredSlides: true
+	});
 
-	// $('body').hide()
 
-})
+
+	document.querySelector('video').playbackRate = 2;
+
+
+});
